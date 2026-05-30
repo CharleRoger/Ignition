00c05b214d6826d8ffd966557bd3cacc158d5f47
aerograch <aerograchalr@gmail.com>
2026-05-30 12:38:05 +0300

    Fix for #8

--- ModuleIgnitionEngineController.cs
+++ ModuleIgnitionEngineController.cs
@@ -212,7 +212,7 @@ namespace Ignition
             if (ModuleIsNull()) return 0;
 
             double isp = ModuleEngines.atmosphereCurve.Curve.keys[0].value;
-            if (!ModuleEngines.useVelCurve && ModuleEngines.atmosphereCurve.Curve.keys.Length > 1) isp = ModuleEngines.atmosphereCurve.Curve.keys[1].value;
+            if (!ModuleEngines.useVelCurve && ModuleEngines.atmosphereCurve.Curve.keys.Length > 1 && ModuleEngines.atmosphereCurve.Curve.keys[1].value != 0) isp = ModuleEngines.atmosphereCurve.Curve.keys[1].value;
             return 1000 * ModuleEngines.maxThrust / (ModuleEngines.g * isp);
         }
 
